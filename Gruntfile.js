43919a55087962bc0c486cd492fa43771e75d4e5
Nazar <naz466@yandex.ru>
2018-10-04 17:01:53 +0300

    first backend files

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    var config = {
+        pkg: grunt.file.readJSON('package.json'),
+        browserify:     {
+            options:      {
+                transform:  [ require('brfs') ],
+                browserifyOptions: {
+                    basedir: "Frontend/src/js/"
+                }
+            },
+
+        }
+    };
+
+    //Налаштування відстежування змін в проекті
+    var watchDebug = {
+        options: {
+            'no-beep': true
+        },
+        //Назва завдання будь-яка
+        scripts: {
+            //На зміни в яких файлах реагувати
+            files: ['Frontend/src/**/*.js', 'Frontend/**/*.ejs', 'Frontend/src/*.js'],
+            //Які завдання виконувати під час зміни в файлах
+            tasks: ['browserify:pizza']
+        }
+    };
+
+
+    //Ініціалузвати Grunt
+    config.watch = watchDebug;
+    grunt.initConfig(config);
+
+    //Сказати які модулі необхідно виокристовувати
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    //Список завданнь по замовчування
+    grunt.registerTask('default',
+        [
+            'browserify:pizza',
+            'browserify:order',
+            'browserify:login'
+            //Інші завдання які необхідно виконати
+        ]
+    );
+
+};

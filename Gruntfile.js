1d4c159156df73c7c9bc5562065aac1c8fcb3589
Nazar <naz466@yandex.ru>
2018-10-04 17:27:01 +0300

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,37 +13,25 @@ module.exports = function(grunt) {
         }
     };
 
-    //Налаштування відстежування змін в проекті
     var watchDebug = {
         options: {
             'no-beep': true
         },
-        //Назва завдання будь-яка
         scripts: {
-            //На зміни в яких файлах реагувати
             files: ['Frontend/src/**/*.js', 'Frontend/**/*.ejs', 'Frontend/src/*.js'],
-            //Які завдання виконувати під час зміни в файлах
-            tasks: ['browserify:pizza']
+            tasks: []
         }
     };
 
-
-    //Ініціалузвати Grunt
     config.watch = watchDebug;
     grunt.initConfig(config);
 
-    //Сказати які модулі необхідно виокристовувати
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-
-    //Список завданнь по замовчування
     grunt.registerTask('default',
         [
-            'browserify:pizza',
-            'browserify:order',
-            'browserify:login'
-            //Інші завдання які необхідно виконати
+
         ]
     );
 
